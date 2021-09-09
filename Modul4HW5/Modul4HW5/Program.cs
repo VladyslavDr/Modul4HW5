1bed7fc73f57ce1149323a971ba5d6053ecd83ba
WIN-T2O982ERQRV\User <vladislav.drushliak@gmail.com>
2021-09-09 15:27:44 +0300

    Create programm

---
+++ Modul4HW5/Modul4HW5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Modul4HW5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
